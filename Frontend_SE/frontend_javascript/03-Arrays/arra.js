eac1fe0cc2e722f7f2781f345113fbefb7d9137d
Meargteame <mearegteame99995555@gmail.com>
2024-10-12 08:30:20 +0300

    Arrays section is very unexpectable

---
+++ Frontend_SE/frontend_javascript/03-Arrays/arra.js
@@ -0,0 +1,23 @@
+
+
+// Accessing the Array using the square brackets 
+// arrayName['index']
+
+// Array methods 
+/**
+ * .length() - returns the length of the array
+ * .indexOf(value) - returns the index of the value where it is in.
+ * .includes("value") - returns a boolean value of true or false.
+ * .pop() - removing the last index of the array.
+ * .push('value') - adding value to the end of the array. and returns the length of the array after the pushing you have made 
+ * 
+ * .sort() - sorts the values of the arrays
+ * .join("value") - joins the array values  do to the indicated thingy in the join() method 
+    ex : join(&) , join(" ")
+ * .concat("value") - concatenates a value with the array.
+ */
+
+
+// Strings in javascript are immutable - the values inside them can not be changed  
+// Arrays in javascript are mutuable - the values inside them can be changed 
+
