1d5bdcd09f39073ee75ae7a395ecce22b8b2d627
Meargteame <mearegteame99995555@gmail.com>
2024-10-14 12:31:11 +0300

    conditionals in javascript uncovered

---
+++ Frontend_SE/frontend_javascript/07-Conditionals/script.js
@@ -0,0 +1,18 @@
+// Conditionals
+    // if statements let us execute code under certain condition 
+        // if / else 
+
+function compare(x,y){
+    if(x >y){
+        console.log(x , "is greater than", y);
+    }
+    else if(x < y){
+        console.log(x , "is less than ", y);
+    }
+    else{
+        console.log(x ,"x is equal to ",y);
+    }
+}
+
+
+compare(2,2);
